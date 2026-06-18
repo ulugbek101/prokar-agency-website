4373925ab55af1d3c1aaeef1be5a57e227440571
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2026-06-18 14:57:23 +0500

    Add staff detail and about views, pass certificates to home context

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,5 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('feedback/', views.feedback_submit, name='feedback_submit'),
+    path('staff/<slug:slug>/', views.staff_detail, name='staff_detail'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,16 +1,32 @@
 from django.contrib import messages
 from django.http import JsonResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext_lazy as _
 
 from apps.blog.models import Post
 from .forms import FeedbackForm
+from .models import Certificate, Staff
 from .telegram import send_feedback_notification
 
 
 def home(request):
     latest_posts = Post.objects.filter(is_active=True).prefetch_related('tags')[:3]
-    return render(request, 'home.html', {'latest_posts': latest_posts})
+    staff_list = Staff.objects.all()
+    certificates = Certificate.objects.filter(is_active=True)
+    return render(request, 'home.html', {
+        'latest_posts': latest_posts,
+        'staff_list': staff_list,
+        'certificates': certificates,
+    })
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def staff_detail(request, slug):
+    staff = get_object_or_404(Staff, slug=slug)
+    return render(request, 'staff/detail.html', {'staff': staff})
 
 
 def feedback_submit(request):
